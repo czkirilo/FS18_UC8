85375c94166e7ffc43b26359f0bafeb043d163d8
czkirilo <caiquez.kirilo@hotmail.com>
2022-10-24 19:23:22 -0300

    Fim da primeira parte da aula

--- Encontro_Remoto_2_UC8.js
+++ Encontro_Remoto_2_UC8.js
@@ -1,12 +1,26 @@
 //Atividade do Encontro Remoto 2
 
 /*
-Percorrer de Zero até o número de alunos 
+OK - Percorrer de Zero até o número de alunos 
 
-Se o número for par, escreva ‘par’ e o número correspondente 
+OK - Se o número for par, escreva ‘par’ e o número correspondente 
 
-Se o número for ímpar, escreva ‘ímpar’ e o número correspondente  
+OK - Se o número for ímpar, escreva ‘ímpar’ e o número correspondente  
 
-Se o número for zero, escreva ‘zero’ e o número correspondente 
+OK - Se o número for zero, escreva ‘zero’ e o número correspondente 
 */
  
+let numeroDeAlunos = 10
+// contador++ eh a mesma coisa que contador = contador + 1 / exemplo: x = 3, x = 3 + 1
+for (let contador = 0; contador <= numeroDeAlunos; contador++){
+    //console.log(contador)
+
+    if (contador == 0){
+        console.log("o numero atual eh zero")
+    }else if (contador % 2 == 0){
+        console.log("O numero "+ contador + " eh PAR")
+    }else{
+        console.log(`O numero ${contador} é IMPAR`)
+    }
+}
+
