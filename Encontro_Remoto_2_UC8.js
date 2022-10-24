13ed7b079c74ba187122370519f78884951d46c4
czkirilo <caiquez.kirilo@hotmail.com>
2022-10-24 18:47:20 -0300

    Arquivo da aula e instrucoes criadas

---
+++ Encontro_Remoto_2_UC8.js
@@ -0,0 +1,12 @@
+//Atividade do Encontro Remoto 2
+
+/*
+Percorrer de Zero até o número de alunos 
+
+Se o número for par, escreva ‘par’ e o número correspondente 
+
+Se o número for ímpar, escreva ‘ímpar’ e o número correspondente  
+
+Se o número for zero, escreva ‘zero’ e o número correspondente 
+*/
+ 
